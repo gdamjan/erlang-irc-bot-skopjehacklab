06b5ab6d5ebe1a7b2203b1305c767e421d34e487
Damjan Georgievski <gdamjan@gmail.com>
2014-02-13 03:30:22 +0100

    bring status + prisutni together
    the code runs two paralel http requests and the waits for both to respond, so it's a bit more complex

--- src/ircbot_plugin_hacklab_status.erl
+++ src/ircbot_plugin_hacklab_status.erl
@@ -5,8 +5,6 @@
 -export([init/1, handle_event/2, terminate/2, handle_call/2, handle_info/2, code_change/3]).
 
 
-
-
 -define(MAXBODY, 10000).
 
 init(_Args) ->
@@ -16,43 +14,100 @@ init(_Args) ->
 
 handle_event(Msg, State) ->
     case Msg of
-        % explicit command to fetch hacklab status from Cosm
-        {in, Ref, [_Nick, _Name, <<"PRIVMSG">>, Channel = <<"#lugola">>, <<"!status">>]} ->
-            Url = <<"https://api.cosm.com/v2/feeds/86779/datastreams/hacklab_status.json">>,
-            F = fun(Answer) -> Ref:privmsg(Channel, Answer) end,
-            spawn(fun() -> fetcher(Url, F) end),
+        {in, IrcBot, [_Nick, _Name, <<"PRIVMSG">>, Channel = <<"#lugola">>, <<"!status">>]} ->
+            doit(IrcBot, Channel),
             {ok, State};
-       _ ->
+        {in, IrcBot, [_Nick, _Name, <<"PRIVMSG">>, Channel = <<"#lugola">>, <<"!prisutni">>]} ->
+            doit(IrcBot, Channel),
+            {ok, State};
+        _ ->
             {ok, State}
     end.
 
-handle_call(_Request, State) -> {ok, ok, State}.
-handle_info(_Info, State) -> {ok, State}.
-code_change(_OldVsn, State, _Extra) -> {ok, State}.
-terminate(_Args, _State) -> ok.
+
+doit(IrcBot, Channel) ->
+    Collector = spawn(fun() ->
+        Responses = wait_for_responses([], 2),
+        Response = << <<T/binary, " ">> || T <- Responses >>,
+        IrcBot:privmsg(Channel, Response)
+    end),
+    spawn(fun() ->
+        Collector ! { prisutni, get_prisutni() }
+    end),
+    spawn(fun() ->
+        Collector ! { status, get_status() }
+    end).
+
+wait_for_responses(Responses, 0) ->
+    Responses;
+
+wait_for_responses(Responses, Needed) ->
+    receive
+        {prisutni, Text} ->
+            wait_for_responses([Text | Responses], Needed - 1);
+        {status, Text} ->
+            wait_for_responses([Text | Responses], Needed - 1)
+    after
+        5000 ->
+            wait_for_responses([<<"some timeout">> | Responses], 0)
+    end.
 
 
+get_prisutni() ->
+    Url = <<"http://status.spodeli.org/status?limit=1">>,
+    Headers = [{<<"User-Agent">>, <<"Mozilla/5.0 (erlang-irc-bot)">>}],
+    Options = [{recv_timeout, 5000}, {follow_redirect, true}],
+    {ok, StatusCode, _RespHeaders, Ref} = hackney:request(get, Url, Headers, <<>>, Options),
+    {ok, Body} = hackney:body(Ref, ?MAXBODY),
+    hackney:close(Ref),
+    case StatusCode of
+        200 ->
+            {Json} = couchbeam_ejson:decode(Body),
+            [{Counter}|_] = proplists:get_value(<<"counters">>, Json),
+            Count = proplists:get_value(<<"count">>, Counter),
+            CountS = integer_to_list(Count),
+            People = proplists:get_value(<<"present">>, Json),
 
-%% The function gets spawned as a separate process,
-%% and fails silently on many errors.
-fetcher(Url, Callback) ->
+            case {Count, People} of
+                {0, _} ->
+                    <<"во хаклаб нема никој :(">>;
+                {_, []} ->
+                    [<<"во хаклаб има ">>, CountS, <<" уреди">>];
+                _ ->
+                    Names = [ proplists:get_value(<<"name">>, Person) || {Person} <- People ],
+                    [<<"Присутни: ">>, ircbot_lib:iolist_join(Names, ", "), <<". Вкупно уреди: ">>, CountS]
+            end;
+        _ ->
+            N = list_to_binary(integer_to_list(StatusCode)),
+            <<"{error ", N/binary, "}">>
+    end.
+
+get_status() ->
+    Url = <<"https://api.cosm.com/v2/feeds/86779/datastreams/hacklab_status.json">>,
     Headers = [{<<"User-Agent">>, <<"Mozilla/5.0 (erlang-irc-bot)">>},
                 {<<"X-ApiKey">>,<<"vqElqXeb7Lu6ZwDElnKQ8XpGMG-SAKxxMHV3YWFoeHE4OD0g">>}],
     Options = [{recv_timeout, 5000}, {follow_redirect, true}],
     {ok, StatusCode, _RespHeaders, Ref} = hackney:request(get, Url, Headers, <<>>, Options),
     {ok, Body} = hackney:body(Ref, ?MAXBODY),
+    hackney:close(Ref),
     case StatusCode of
         200 ->
             {Json} = couchbeam_ejson:decode(Body),
             Current_Value = proplists:get_value(<<"current_value">>, Json),
             case Current_Value of
                 <<"0">> ->
-                    Callback(<<"Хаклабот е затворен. :(">>);
+                    <<"Хаклабот е затворен. :(">>;
                 <<"1">> ->
-                    Callback(<<"Хаклабот е отворен. Дојди! (http://status.spodeli.org)">>)
+                    <<"Хаклабот е отворен. Дојди! (http://status.spodeli.org)">>
             end;
         _ ->
             N = list_to_binary(integer_to_list(StatusCode)),
-            Callback(<<"{error ", N/binary, "}">>)
-    end,
-    hackney:close(Ref).
+            <<"{error ", N/binary, "}">>
+    end.
+
+
+
+handle_call(_Request, State) -> {ok, ok, State}.
+handle_info(_Info, State) -> {ok, State}.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+terminate(_Args, _State) -> ok.
