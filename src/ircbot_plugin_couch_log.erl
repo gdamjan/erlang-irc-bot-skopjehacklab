3cf2aba7d1801eff4ba1119b66feeb13a51cca67
Damjan Georgievski <gdamjan@gmail.com>
2014-04-15 01:57:27 +0200

    the .hrl file for couchbeam hasn't been needed for some time

--- src/ircbot_plugin_couch_log.erl
+++ src/ircbot_plugin_couch_log.erl
@@ -4,8 +4,6 @@
 -behaviour(gen_event).
 -export([init/1, handle_event/2, terminate/2, handle_call/2, handle_info/2, code_change/3]).
 
--include_lib("couchbeam/include/couchbeam.hrl").
-
 %% This plugin requires couchbeam (http://benoitc.github.com/couchbeam/)
 
 %% Configuration (settings.cfg):
