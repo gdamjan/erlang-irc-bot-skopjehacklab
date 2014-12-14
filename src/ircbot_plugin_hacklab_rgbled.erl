6112df3612d83b2ef4153762d538686ebcdab534
Damjan Georgievski <gdamjan@gmail.com>
2014-12-14 16:40:46 +0100

    don't use DNAT, use nginx proxy

--- src/ircbot_plugin_hacklab_rgbled.erl
+++ src/ircbot_plugin_hacklab_rgbled.erl
@@ -18,7 +18,7 @@ handle_event(Msg, State) ->
     case Msg of
         % explicit command to make an api call for the RGB
         {in, Ref, [Nick, _Name, <<"PRIVMSG">>, <<"#lugola">>, <<"!алохаклаб">>]} ->
-            Url = <<"http://hacklab.ie.mk:5000/api/blink">>,
+            Url = <<"http://hacklab.ie.mk/api/blink">>,
             spawn_link(fun() ->
                 Answer = fetcher(Url),
                 Ref:notice(Nick, Answer)
