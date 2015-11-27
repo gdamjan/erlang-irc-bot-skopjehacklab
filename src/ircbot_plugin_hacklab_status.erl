3379a8c1839c8de275a05fae0184cf28da7aeb98
Damjan Georgievski <gdamjan@gmail.com>
2015-11-27 19:00:59 +0100

    rework the !статус command
    gets data from http://hacklab.ie.mk/status/ and https://db.softver.org.mk/influxdb/

--- src/ircbot_plugin_hacklab_status.erl
+++ src/ircbot_plugin_hacklab_status.erl
@@ -3,7 +3,7 @@
 
 -behaviour(gen_event).
 -export([init/1, handle_event/2, terminate/2, handle_call/2, handle_info/2, code_change/3]).
--export([status_loop/1]).
+-export([status_loop/1, get_status/0, get_temps/0]).
 
 
 -define(MAXBODY, 10000).
@@ -30,80 +30,84 @@ handle_event(Msg, State) ->
 
 
 doit(IrcBot, Channel) ->
-    Collector = spawn(fun() ->
-        Response = wait_for_responses([], 2),
-        IrcBot:privmsg(Channel, Response)
-    end),
-    spawn(fun() ->
-        Collector ! { prisutni, get_prisutni() }
-    end),
-    spawn(fun() ->
-        Collector ! { status, get_status() }
-    end).
+  spawn(fun() ->
+    Job1 = rpc:async_call(node(), ?MODULE, get_status, []),
+    Job2 = rpc:async_call(node(), ?MODULE, get_temps, []),
 
-wait_for_responses(Responses, 0) ->
-    Responses ++ [<<" (http://status.spodeli.org)">>];
+    % TODO handle timeouts of yield and hackney errors
+    Status = case rpc:yield(Job1) of
+      {error, ErrMsg1} ->
+        <<"Не се знае дали е отворено ("/utf8, ErrMsg1/binary,").">>;
+      <<"CLOSED">> ->
+        <<"Хаклабот е затворен. :("/utf8>>;
+      <<"OPEN">> ->
+        <<"Хаклабот е отворен. Дојди!"/utf8>>
+    end,
 
-wait_for_responses(Responses, Needed) ->
-    receive
-        {prisutni, Text} ->
-            wait_for_responses([Text, " " | Responses], Needed - 1);
-        {status, Text} ->
-            wait_for_responses([Text, " " | Responses], Needed - 1)
-    after
-        5000 ->
-            wait_for_responses([<<"some timeout">> | Responses], 0)
-    end.
+    Temperature = case rpc:yield(Job2) of
+      {error, ErrMsg2} ->
+        <<"Температури: непознато ("/utf8, ErrMsg2/binary, ")."/utf8>>;
+      Temps ->
+        Temps1 = [float_to_binary(float(T), [{decimals,2}]) || T <- Temps],
+        Temps2 = hackney_bstr:join(Temps1, ", "),
+        <<"Температури: "/utf8, Temps2/binary, ".">>
+    end,
 
-
-get_prisutni() ->
-    Url = <<"http://status.spodeli.org/status?limit=1">>,
-    Headers = [{<<"User-Agent">>, <<"Mozilla/5.0 (erlang-irc-bot)">>}],
-    Options = [{recv_timeout, 5000}, {follow_redirect, true}],
-    {ok, StatusCode, _RespHeaders, Ref} = hackney:request(get, Url, Headers, <<>>, Options),
-    {ok, Body} = hackney:body(Ref, ?MAXBODY),
-    hackney:close(Ref),
-    case StatusCode of
-        200 ->
-            {Json} = couchbeam_ejson:decode(Body),
-            [{Counter}|_] = proplists:get_value(<<"counters">>, Json),
-            Count = proplists:get_value(<<"count">>, Counter),
-            CountS = list_to_binary(integer_to_list(Count)),
-            People = proplists:get_value(<<"present">>, Json),
-
-            case {Count, People} of
-                {0, _} ->
-                    <<"Во хаклаб нема никој :("/utf8>>;
-                {_, []} ->
-                    [<<"Во хаклаб има "/utf8>>, CountS, <<" уреди."/utf8>>];
-                _ ->
-                    Names = [ proplists:get_value(<<"name">>, Person) || {Person} <- People ],
-                    [<<"Присутни: "/utf8>>, ircbot_lib:iolist_join(Names, ", "), <<". Вкупно уреди: "/utf8>>, CountS, <<".">>]
-            end;
-        _ ->
-            N = list_to_binary(integer_to_list(StatusCode)),
-            <<"{error ", N/binary, "}">>
-    end.
+    Response = hackney_bstr:join([Status, Temperature, <<"(http://status.spodeli.org/)"/utf8>>], " "),
+    IrcBot:privmsg(Channel, Response)
+  end).
 
 get_status() ->
     Url = <<"http://hacklab.ie.mk/status/">>,
     Options = [{recv_timeout, 5000}, {follow_redirect, true}],
-    {ok, StatusCode, _RespHeaders, Ref} = hackney:request(get, Url, [], <<>>, Options),
-    {ok, Body} = hackney:body(Ref, ?MAXBODY),
-    hackney:close(Ref),
-    case StatusCode of
-        200 ->
-            {match, [Status]} = re:run(Body, <<"^status: (\\w+)">>, [caseless, multiline, {capture, [1], binary}]),
-            case Status of
-                <<"CLOSED">> ->
-                    <<"Хаклабот е затворен. :("/utf8>>;
-                <<"OPEN">> ->
-                    <<"Хаклабот е отворен. Дојди!"/utf8>>
-            end;
-        _ ->
-            N = list_to_binary(integer_to_list(StatusCode)),
-            <<"{error ", N/binary, "}">>
+    case hackney:request(get, Url, [], <<>>, Options) of
+      {ok, StatusCode, _RespHeaders, Ref} ->
+        get_status_result(StatusCode, Ref);
+      {error, Error} ->
+        {error, atom_to_binary(Error, utf8)}
     end.
+
+get_status_result(StatusCode, Ref) ->
+  {ok, Body} = hackney:body(Ref, ?MAXBODY),
+  hackney:close(Ref),
+  case StatusCode of
+    200 ->
+      {match, [Status]} = re:run(Body, <<"^status: (\\w+)">>, [caseless, multiline, {capture, [1], binary}]),
+      Status;
+    _ ->
+      ErrMsg = list_to_binary(integer_to_list(StatusCode)),
+      {error, <<"http:", ErrMsg/binary>>}
+  end.
+
+get_temps() ->
+  DbUrl = <<"https://db.softver.org.mk/influxdb/">>,
+  Path = <<"query">>,
+  Query = [{<<"db">>, <<"status">>},
+    {<<"q">>, <<"SELECT outside,hardware_room,random_room,lounge_area FROM temperatures ORDER BY time DESC LIMIT 1">>}],
+  Url = hackney_url:make_url(DbUrl, Path, Query),
+  Options = [{recv_timeout, 5000}, {follow_redirect, true}],
+  case hackney:request(get, Url, [], <<>>, Options) of
+    {ok, StatusCode, _RespHeaders, Ref} ->
+      get_temps_result(StatusCode, Ref);
+    {error, Error} ->
+      {error, atom_to_binary(Error, utf8)}
+  end.
+
+get_temps_result(StatusCode, Ref) ->
+  {ok, Body} = hackney:body(Ref),
+  hackney:close(Ref),
+  case StatusCode of
+    200 ->
+      {Json} = couchbeam_ejson:decode(Body),
+      [{Result0}|_] = proplists:get_value(<<"results">>, Json),
+      [{Serie0}|_] = proplists:get_value(<<"series">>, Result0),
+      [[_Timestamp|Temps]] = proplists:get_value(<<"values">>, Serie0),
+      Temps;
+    _ ->
+      ErrMsg = list_to_binary(integer_to_list(StatusCode)),
+      {error, <<"http:", ErrMsg/binary>>}
+  end.
+
 
 status_loop(LastStatus) ->
     Url = <<"http://hacklab.ie.mk/status/open">>,
